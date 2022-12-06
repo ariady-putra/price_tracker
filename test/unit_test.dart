7a0c453f8ed404f29a13492ade9899f887b61667
Ariady Putra <ariady.putra@gmail.com>
2022-12-07 02:15:47 +0700

    Add unit tests

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -4,12 +4,48 @@
 // writing unit tests, visit
 // https://flutter.dev/docs/cookbook/testing/unit/introduction
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:price_tracker/src/price_tracker/cubit/cubit.dart';
 
 void main() {
-  group('Plus Operator', () {
-    test('should add two numbers together', () {
-      expect(1 + 1, 2);
-    });
-  });
+  group(
+    'Price Up',
+    () {
+      test(
+        'should be green',
+        () {
+          PriceCubit priceCubit = PriceCubit();
+
+          priceCubit.updatePrice('', 50);
+          priceCubit.updatePrice('', 75);
+
+          expect(
+            priceCubit.state.priceTextColor,
+            Colors.green,
+          );
+        },
+      );
+    },
+  );
+
+  group(
+    'Price Down',
+    () {
+      test(
+        'should be red',
+        () {
+          PriceCubit priceCubit = PriceCubit();
+
+          priceCubit.updatePrice('', 50);
+          priceCubit.updatePrice('', 25);
+
+          expect(
+            priceCubit.state.priceTextColor,
+            Colors.red,
+          );
+        },
+      );
+    },
+  );
 }
